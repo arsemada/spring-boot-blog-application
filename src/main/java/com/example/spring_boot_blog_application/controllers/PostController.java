0470262dbbe7d4d1d67f1bed7e85425814e0f552
Arsema Addis <arsemadd@gmail.com>
2025-04-25 13:05:38 +0300

    adding /post/new POST in PostController

--- src/main/java/com/example/spring_boot_blog_application/controllers/PostController.java
+++ src/main/java/com/example/spring_boot_blog_application/controllers/PostController.java
@@ -1,12 +1,16 @@
 package com.example.spring_boot_blog_application.controllers;
 
+import com.example.spring_boot_blog_application.models.Account;
 import com.example.spring_boot_blog_application.models.Post;
+import com.example.spring_boot_blog_application.services.AccountService;
 import com.example.spring_boot_blog_application.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -16,8 +20,11 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private AccountService accountService;
+
     @GetMapping("/posts/{id}")
-    public String getPost(@PathVariable Long id, Model model){
+    public String getPost(@PathVariable Long id, Model model) {
         Optional<Post> optionalPost = postService.getById(id);
         if (optionalPost.isPresent()) {
             Post post = optionalPost.get();
@@ -26,5 +33,24 @@ public class PostController {
         } else {
             return "404";
         }
+    }
+
+    @GetMapping("/posts/new")
+    public String createNewPost(Model model) {
+        Optional<Account> optionalAccount = accountService.findByEmail("user.user@domain.com");
+        if (optionalAccount.isPresent()) {
+            Post post = new Post();
+            post.setAccount(optionalAccount.get());
+            model.addAttribute("post", post);
+            return "post_new";
+        } else {
+            return "404";
+        }
+    }
+
+    @PostMapping("/posts/new")
+    public String saveNewPost(@ModelAttribute Post post) {
+        postService.save(post);
+        return "redirect:/posts/" + post.getId();
     }
 }
